6e50adfdd09146aa60f4a279061ef5359fe89529
maxpiorischin <maxpiorischin@hotmail.com>
2021-12-01 16:39:38 -0500

    Test Modification

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          CPS530 React and Express WebApp! {"\n"}
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a

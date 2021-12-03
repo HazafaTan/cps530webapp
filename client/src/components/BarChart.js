09f040ed966b106586e00456961031340b6927ea
James <james.ttan64@gmail.com>
2021-12-03 01:42:49 -0500

    .

---
+++ client/src/components/BarChart.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+import { Chart } from 'react-chartjs-2'
+
+const BarChart = () => {   
+    return (
+        <div>
+        {/*<Bar
+            data = {{
+                labels : ['Small-business','Mid-Market','Enterprise'],
+                
+                datasets: [
+                    {
+                    label: '# of people',
+                    backgroundColor: '#rgba(75,192,192,1)',
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 2, 
+                    data: [24,8,4]
+                    }
+                ]
+                
+            }}
+            />*/}
+
+
+        </div>
+    )
+}
+export default BarChart

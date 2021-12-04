c9a26dc8a1c76bc76a636bc33773aaca150e460e
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-12-04 16:43:38 -0500

    fixed debounce

--- client/src/components/FrontDemo.js
+++ client/src/components/FrontDemo.js
@@ -9,21 +9,20 @@ const default_img = "https://images.unsplash.com/photo-1581081536310-c88f076b4ac
 const unsplah_api = (search_term) => `https://source.unsplash.com/800x450/?${encodeURI(search_term)}`;
 
 // Simple debounce funciton from: https://davidwalsh.name/javascript-debounce-function
-export function debounce(func, wait, immediate) {
-    let timeout;
-    return () => {
-      let context = this,
-        args = arguments;
-      let later = () => {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-}
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 const cardstyle = {
     backgroundColor: '#018786',
@@ -37,10 +36,11 @@ export default class FrontDemo extends Component {
             img_url: default_img, 
             loading: false
         };
-        this.updateSearch = debounce((event) => {
+        this.pop = (event) => {
             this.setState({search_term: event.target.value});
             this.apiReq();
-        }, 800);
+        };
+        this.updateSearch = debounce(this.pop, 800);
     }
 
     apiReq() {

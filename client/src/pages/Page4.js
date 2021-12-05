f1e02509e5575abf06cfba73282dff28a901cd4d
Haz <48661942+HazafaTan@users.noreply.github.com>
2021-12-05 15:58:12 -0500

    Updated Page4.js

--- client/src/pages/Page4.js
+++ client/src/pages/Page4.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from './../logo.svg';
 import BackDemo from "../components/BackDemo"
+import FrontDemo from "../components/FrontDemo"
+
 import './../App.css';
 import theme from "../theme";
 import {ThemeProvider} from '@mui/material/styles';
@@ -8,7 +10,6 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid'; 
 
 const image = {
@@ -20,12 +21,28 @@ const cardstyle = {
   backgroundColor: '#018786',
 };
 
+function CustomCard({title, image, body}) {
+  return (
+    <Card sx={{ maxWidth: 345 }} style={cardstyle}>
+      <CardMedia component="img" height="140" image={image} alt={title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {body}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
 
 export default function Page4() {
   return (
     <div className="App">
       <header className="App-header">
-      <ThemeProvider theme= {theme}>
+      <ThemeProvider theme={theme}>
         <img src={logo} className="App-logo" alt="logo" style={image}  />
         <h1>
           CPS530 React and Express WebApp! {"\n"}
@@ -61,4 +78,3 @@ export default function Page4() {
     </div>
   );
 }
-
